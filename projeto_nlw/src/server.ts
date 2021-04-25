6fa903317d341f2dfe620dfa11333f903ca9d4f3
daniloandrade47 <danilo.andrade47@gmail.com>
2021-04-25 00:08:48 -0300

    Aula 02

--- projeto_nlw/src/server.ts
+++ projeto_nlw/src/server.ts
@@ -9,3 +9,31 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
+
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deletar
+ * PATCH = Alterar uma informação específica
+ */
+
+
+/**
+import express, { request, response } from "express";
+
+app.get("/", (request, response) => {
+  //return response.send("Olá NLW 05");
+  return response.json({
+    message: "Olá NLW 05!",
+  })
+});
+
+app.post("/", (request, response) =>{
+  return response.json({
+    message:"Usuário salvo com sucesso!"
+  })
+});
+
+ */
+
